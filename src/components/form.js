f4eb20e3bb2a369065c921db7f61b64848f96069
Nick <nikitalytvynov0506@gmail.com>
2024-01-03 19:52:03 +0200

    sii

---
+++ src/components/form.js
@@ -0,0 +1,87 @@
+import React from "react";
+import { ReactDOM } from "react";
+import { useFormik } from "formik";
+//import * as Yup from "yup";
+
+const validate = (values) => {
+  const errors = {};
+
+  if (!values.firstName) {
+    errors.firstName = "Required";
+  } else if (values.firstName.length > 15) {
+    errors.firstName = "Must be 15 characters or less";
+  }
+
+  if (!values.phone) {
+    errors.phone = "Required";
+  } else if (values.phone.length > 15) {
+    errors.phone = "Must not to be more than 15 numbers";
+  }
+
+  if (!values.email) {
+    errors.email = "Required";
+  } else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(values.email)) {
+    errors.email = "Invalid email address";
+  }
+
+  return errors;
+};
+
+const SignupForm = () => {
+  const formik = useFormik({
+    initialValues: {
+      firstName: "",
+      phone: "",
+      email: "",
+    },
+    validate,
+    onSubmit: (values) => {
+      alert(JSON.stringify(values, null, 2));
+    },
+  });
+  return (
+    <form onSubmit={formik.handleSubmit}>
+      <label htmlFor="firstName">First Name</label>
+      <input
+        id="firstName"
+        name="firstName"
+        type="text"
+        onChange={formik.handleChange}
+        onBlur={formik.handleBlur}
+        value={formik.values.firstName}
+      />
+      {formik.errors.firstName ? <div>{formik.errors.firstName}</div> : null}
+
+      <label htmlFor="phone">Phone</label>
+      <input
+        id="phone"
+        name="phone"
+        type="phone"
+        onChange={formik.handleChange}
+        onBlur={formik.handleBlur}
+        value={formik.values.phone}
+      />
+      {formik.errors.phone ? <div>{formik.errors.phone}</div> : null}
+
+      <label htmlFor="email">Email Address</label>
+      <input
+        id="email"
+        name="email"
+        type="email"
+        onChange={formik.handleChange}
+        onBlur={formik.handleBlur}
+        value={formik.values.email}
+      />
+      {formik.errors.email ? <div>{formik.errors.email}</div> : null}
+
+      <button type="submit">Submit</button>
+    </form>
+  );
+};
+
+function App() {
+  return <SignupForm />;
+}
+
+const rootElement = document.getElementById("root");
+ReactDOM.render(<App />, rootElement);
